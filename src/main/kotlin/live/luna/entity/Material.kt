e4bf3f16b7a02e7c1b7ab3874ed218512db87294
igor <yakovlev1996@gmail.com>
2018-03-25 01:15:55 +0300

    annotated material model

--- src/main/kotlin/live/luna/entity/Material.kt
+++ src/main/kotlin/live/luna/entity/Material.kt
@@ -1,19 +1,25 @@
 package live.luna.entity
 
+import live.luna.GraphQLField
+import live.luna.GraphQLObject
 import javax.persistence.*
 
 @Entity
 @Table(name = "material")
+@GraphQLObject
 data class Material(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id", nullable = false)
+        @GraphQLField
         val id: Long = 0,
 
         @Column(name = "firm", nullable = false, columnDefinition = "TEXT")
+        @GraphQLField
         val firm: String,
 
         @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+        @GraphQLField
         val description: String
 
 ) {
