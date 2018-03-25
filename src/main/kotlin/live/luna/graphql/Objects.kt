e40f20661ba00e314940bafcec77f642e09125d3
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-03-25 13:28:20 +0300

    + feed query (that's genius)

--- src/main/kotlin/live/luna/graphql/Objects.kt
+++ src/main/kotlin/live/luna/graphql/Objects.kt
@@ -7,6 +7,11 @@ lateinit var masterService: MasterService
 
 @GraphQLObject
 class Query {
+    @GraphQLField(of = Master::class)
+    fun feed(@Argument("limit") limit: Int, @Argument("offset") offset: Int): List<Master> {
+        return masterService.getList(limit, offset)
+    }
+
     @GraphQLField(nullable = true)
     fun master(@Argument("id") id: Long): Master? {
         return masterService.getById(id)
