081c38e510c4cc2c92b04548ad289494bd85bad3
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-04-04 23:17:46 +0300

    Adding and removing services

--- src/main/kotlin/live/luna/service/impl/ServiceServiceImpl.kt
+++ src/main/kotlin/live/luna/service/impl/ServiceServiceImpl.kt
@@ -1,15 +1,27 @@
 package live.luna.service.impl
 
 import live.luna.dao.ServiceDao
+import live.luna.entity.Material
+import live.luna.entity.Photo
+import live.luna.graphql.UserContext
+import live.luna.service.MasterService
 import live.luna.service.ServiceService
+import live.luna.service.ServiceTypeService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class ServiceServiceImpl : ServiceService {
 
     @Autowired
     private lateinit var serviceDao: ServiceDao
+
+    @Autowired
+    private lateinit var masterService: MasterService
+
+    @Autowired
+    private lateinit var serviceTypeService: ServiceTypeService
 
     override fun insert(service: live.luna.entity.Service) {
         serviceDao.insert(service)
@@ -25,5 +37,43 @@ class ServiceServiceImpl : ServiceService {
 
     override fun getById(id: Long): live.luna.entity.Service? {
         return serviceDao.getById(id)
+    }
+
+    override fun addService(context: UserContext, typeId: Long, price: BigDecimal, description: String,
+                            duration: Long, materials: List<Material>?, photos: List<Photo>?): live.luna.entity.Service? {
+        if (context.user == null) {
+            return null;
+        }
+
+        val master = masterService.getByUserId(context.user.id) ?: return null // wtf?
+        val serviceType = serviceTypeService.getById(typeId) ?: return null
+
+        val service = live.luna.entity.Service(
+                master = master,
+                type = serviceType,
+                price = price,
+                description = description,
+                duration = duration,
+                materials = materials ?: emptyList(),
+                photos = photos ?: emptyList()
+        )
+        insert(service)
+        return service
+    }
+
+    override fun removeService(serviceId: Long, context: UserContext): live.luna.entity.Service? {
+        if (context.user == null) {
+            return null
+        }
+
+        val master = masterService.getByUserId(context.user.id) ?: return null // wtf?
+        val service = getById(serviceId) ?: return null
+
+        if (service.master.id != master.id) {
+            return null
+        }
+
+        delete(service)
+        return service
     }
 }
