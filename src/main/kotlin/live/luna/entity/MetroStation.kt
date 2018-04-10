81961a9a023f5f38bf93a0ee09e2f8545b8cb638
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-04-10 23:46:05 +0300

    Fixed ID types

--- src/main/kotlin/live/luna/entity/MetroStation.kt
+++ src/main/kotlin/live/luna/entity/MetroStation.kt
@@ -8,7 +8,7 @@ data class MetroStation(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(name = "id", nullable = false)
-        val id: Int = 0,
+        val id: Long = 0,
 
         @Column(name = "name", nullable = false)
         val name: String,
